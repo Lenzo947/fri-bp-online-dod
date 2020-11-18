ed53331277f7672c760aceff0bd0b01ee001b90c
Lenzo947 <mario.puspoky@gmail.com>
2020-11-18 18:48:02 +0100

    -Added client-side form validation

--- client/BP_OnlineDOD/Message/Message.cs
+++ client/BP_OnlineDOD/Message/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace BP_OnlineDOD.Message
     {
         public virtual int Id { get; set; }
 
+        [Required]
+        [StringLength(100000, ErrorMessage = "Správa musí obsahovať aspoň 5 znakov!", MinimumLength = 5)]
         public virtual string Text { get; set; }
 
         public virtual int ThumbsUpCount { get; set; }
