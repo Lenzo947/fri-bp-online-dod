5709cc24318dbc47a20a0a62e0c229752af6aea9
Lenzo947 <mario.puspoky@gmail.com>
2021-01-01 18:02:44 +0100

    - Dockerized the entire app using docker-compose.yml - Turned BP_OnlineDOD.Shared into a NuGet package (it stays in the solution explorer in case of future changes and repackaging)

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -18,58 +18,66 @@ using Newtonsoft.Json.Serialization;
 
 namespace BP_OnlineDOD.Server
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<OnlineDODContext>(opt => opt.UseSqlServer
-                (Configuration.GetConnectionString("OnlineDODConnection")));
-
-            services.AddControllers().AddNewtonsoftJson(s =>
-            {
-                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                s.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
-
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddScoped<IOnlineDOD, SqlOnlineDOD>();
-
-            services.AddCors(setup =>
-            {
-                setup.AddPolicy("AllowBlazorWasm", policy =>
-                {
-                    policy.WithOrigins(new string[] { "https://localhost:44329" }).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-                });
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseCors("AllowBlazorWasm");
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      var server = Configuration["DBServer"] ?? "localhost";
+      var port = Configuration["DBPort"] ?? "1433";
+      var user = Configuration["DBUser"] ?? "SA";
+      var password = Configuration["DBPassword"] ?? "FRIUniza1990";
+      var database = Configuration["Database"] ?? "OnlineDOD_DB";
+
+      services.AddDbContext<OnlineDODContext>(opt => opt.UseSqlServer
+          ($"Server={server},{port};Initial Catalog={database};User ID ={user};Password={password}"));
+
+      services.AddControllers().AddNewtonsoftJson(s =>
+      {
+        s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        s.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      });
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+      services.AddScoped<IOnlineDOD, SqlOnlineDOD>();
+
+      services.AddCors(setup =>
+      {
+        setup.AddPolicy("AllowBlazorWasm", policy =>
+              {
+                policy.WithOrigins(new string[] { "https://localhost:44329", "http://localhost:5080" }).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+              });
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      PrepDB.PrepDatabase(app);
+
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseCors("AllowBlazorWasm");
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
