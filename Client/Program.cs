5eb5a14d90d4c6e7dcd18bce512628872e0ee58f
Lenzo947 <mario.puspoky@gmail.com>
2021-01-15 18:13:47 +0100

    + Added an authentication service + Updated NuGet package (BP_OnlineDOD.Shared) to accomodate for the service + Small changes to the CSS + Modified home page to be the user status hub

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BP_OnlineDOD.Client.Helpers;
+using BP_OnlineDOD.Client.Services;
 
 namespace BP_OnlineDOD.Client
 {
@@ -17,9 +19,31 @@ namespace BP_OnlineDOD.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services
+                .AddScoped<IAuthenticationService, AuthenticationService>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<IHttpService, HttpService>()
+                .AddScoped<ILocalStorageService, LocalStorageService>();
 
-            await builder.Build().RunAsync();
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //await builder.Build().RunAsync();
+
+            builder.Services.AddScoped(x => {
+                var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+
+                // use fake backend if "fakeBackend" is "true" in appsettings.json
+                if (builder.Configuration["fakeBackend"] == "true")
+                    return new HttpClient(new FakeBackendHandler()) { BaseAddress = apiUrl };
+
+                return new HttpClient() { BaseAddress = apiUrl };
+            });
+
+            var host = builder.Build();
+
+            var authenticationService = host.Services.GetRequiredService<IAuthenticationService>();
+            await authenticationService.Initialize();
+
+            await host.RunAsync();
         }
     }
 }
